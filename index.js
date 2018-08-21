4dc7cb56a25135fc04163376fe890d30978aeaea
hadimaster <hadimaster65555@gmail.com>
2018-08-21 14:39:59 +0700

    add javascript file for index page

--- index.js
+++ index.js
@@ -29,28 +29,3 @@ app.get('/index', (request, response) => {
 app.get('/contact', (request, response) => {
     response.sendFile(path.resolve(__dirname,'contact.html'));
 });
-
-// const http = require('http');
-
-// const fs = require('fs');
-
-// const aboutPage = fs.readFileSync('about.html');
-// const contactPage = fs.readFileSync('contact.html');
-// const homePage = fs.readFileSync('index.html');
-
-// const server = http.createServer((request, response) => {
-//  console.log(request.url);
-//  if (request.url === '/about') {
-//     return response.end(aboutPage);
-//  } else if (request.url === '/contact') {
-//     return response.end(contactPage);
-//  } else if (request.url === '/'){
-//     return response.end(homePage);
-//  } else {
-//     response.writeHead(404);
-//     response.end('Page Not Found');
-//  }
-
-// })
-
-// server.listen(3000);
