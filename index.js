b8bdbe91af98cbbb4b5f5b4ec1fc5e3a22c781c6
hadimaster <hadimaster65555@gmail.com>
2018-08-21 14:16:38 +0700

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-const http = require('http');
+const express = require('express');
 
-const fs = require('fs');
+const app = express();
 
-const aboutPage = fs.readFileSync('about.html');
-const contactPage = fs.readFileSync('contact.html');
-const homePage = fs.readFileSync('index.html');
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
+});
 
-const server = http.createServer((request, response) => {
- console.log(request.url);
- if (request.url === '/about') {
-    return response.end(aboutPage);
- } else if (request.url === '/contact') {
-    return response.end(contactPage);
- } else if (request.url === '/'){
-    return response.end(homePage);
- } else {
-    response.writeHead(404);
-    response.end('Page Not Found');
- }
+app.get('/', (request, response) => {
+    response.json({
+        name: 'hadimaster'
+    });
+});
 
+app.get('/about', (request, response) => {
+    response.send({
+        name: 'hadimaster is cool'
+    })
 })
 
-server.listen(3000);
+// const http = require('http');
+
+// const fs = require('fs');
+
+// const aboutPage = fs.readFileSync('about.html');
+// const contactPage = fs.readFileSync('contact.html');
+// const homePage = fs.readFileSync('index.html');
+
+// const server = http.createServer((request, response) => {
+//  console.log(request.url);
+//  if (request.url === '/about') {
+//     return response.end(aboutPage);
+//  } else if (request.url === '/contact') {
+//     return response.end(contactPage);
+//  } else if (request.url === '/'){
+//     return response.end(homePage);
+//  } else {
+//     response.writeHead(404);
+//     response.end('Page Not Found');
+//  }
+
+// })
+
+// server.listen(3000);
